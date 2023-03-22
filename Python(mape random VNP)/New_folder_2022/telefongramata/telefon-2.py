655341ded09c9a1cf170df93333e579588e77ee3
PufSunCat <114050978+PufSunCat@users.noreply.github.com>
2023-03-22 08:40:03 +0200

    Update telefon-2.py

--- Python(mape random VNP)/New_folder_2022/telefongramata/telefon-2.py
+++ Python(mape random VNP)/New_folder_2022/telefongramata/telefon-2.py
@@ -1,9 +1,10 @@
 import json
+import csv
 
-contacts = []
+contacts_list = []
 
 def LoadContacts():    
-    with open('prog1-10-0810/contacts.json', 'r', encoding='UTF8') as file:        
+    with open('New_folder_2022/telefongramata/contacti.json', 'r', encoding='UTF8') as file:        
         data = json.load(file)
         global contacts
         contacts = data['contacts_list']    
@@ -27,19 +28,54 @@ def AddContact():
 
     contacts.append(contact)
 
-def SaveContacts():
-    with open('prog1-10-0810/contacts.json', 'w', encoding='UTF8') as file:
+def DeleteContact():
+    name = input('Enter name: ')
+    found = False
+
+    for contact in contacts:
+        if contact['name'] == name:
+            contacts.remove(contact)        
+            found = True
+            break
+
+    if found:
+        print(f"{name} has been deleted.")
+    else:
+        print(f"{name} not found.")
+
+def SaveContacts(format):
+    if format == 'json':
+        SaveContactsJson()
+    elif format == 'csv':
+        SaveContactsCSV()
+    else:
+        print('No file saved. File format not defined.')
+
+def SaveContactsJson():
+    with open('New_folder_2022/telefongramata/contacti.json', 'w', encoding='UTF8') as file:
         contacts_dict = {'contacts_list': contacts}
         json.dump(contacts_dict, file, indent=4)
 
+def SaveContactsCSV():
+    with open('New_folder_2022/telefongramata/contacti.csv', mode='w', newline='', encoding='UTF8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['name', 'surname', 'number', 'email'])
+        for contact in contacts:
+            writer.writerow([contact['name'], contact['surname'], contact['number'], contact['email']])
+
 LoadContacts()
 while(True):
-    response = input('A-add new, P-print, E-exit: ')
+    response = input('A-add new, D-delete,P-print, S-save as CSV, E-exit: ')
     if response == 'A':        
         AddContact()
-        SaveContacts()
+        SaveContacts('json')             
     elif response == 'P':
         PrintContacts()
+    elif response == 'D':
+        DeleteContact()
+        SaveContacts('json')       
+    elif response == 'S':
+        SaveContactsCSV()
     elif response == 'E':
         print('Bye bye!')
         break
